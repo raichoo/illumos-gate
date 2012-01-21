38ba4e3c965d33fcbe8911423287e8284253556f
raichoo <raichoo@googlemail.com>
2012-01-21 22:10:01 +0100

    WIP

--- usr/src/uts/common/io/drm/drm_crtc.c
+++ usr/src/uts/common/io/drm/drm_crtc.c
@@ -34,7 +34,7 @@
 #include "drm_mode.h"
 #include "drmP.h"
 #include "drm_crtc.h"
-//#include "drm_edid.h"
+#include "drm_edid.h"
 #include "drm_linux_list.h"
 
 #include <sys/mutex.h>
@@ -2455,33 +2455,33 @@ done:
 	return ret;
 }
 
-//int drm_mode_connector_update_edid_property(struct drm_connector *connector,
-//					    struct edid *edid)
-//{
-//	
-//	struct drm_device *dev = connector->dev;
-//	int ret = 0, size;
-//	
-//	if (connector->edid_blob_ptr)
-//		drm_property_destroy_blob(dev, connector->edid_blob_ptr);
-//
-//	/* Delete edid, when there is none. */
-//	if (!edid) {
-//		connector->edid_blob_ptr = NULL;
-//		ret = drm_connector_property_set_value(connector, dev->mode_config.edid_property, 0);
-//		return ret;
-//	}
-//
-//	size = EDID_LENGTH * (1 + edid->extensions);
-//	connector->edid_blob_ptr = drm_property_create_blob(connector->dev,
-//							    size, edid);
-//
-//	ret = drm_connector_property_set_value(connector,
-//					       dev->mode_config.edid_property,
-//					       connector->edid_blob_ptr->base.id);
-//
-//	return ret;
-//}
+int drm_mode_connector_update_edid_property(struct drm_connector *connector,
+					    struct edid *edid)
+{
+	
+	struct drm_device *dev = connector->dev;
+	int ret = 0, size;
+	
+	if (connector->edid_blob_ptr)
+		drm_property_destroy_blob(dev, connector->edid_blob_ptr);
+
+	/* Delete edid, when there is none. */
+	if (!edid) {
+		connector->edid_blob_ptr = NULL;
+		ret = drm_connector_property_set_value(connector, dev->mode_config.edid_property, 0);
+		return ret;
+	}
+
+	size = EDID_LENGTH * (1 + edid->extensions);
+	connector->edid_blob_ptr = drm_property_create_blob(connector->dev,
+							    size, edid);
+
+	ret = drm_connector_property_set_value(connector,
+					       dev->mode_config.edid_property,
+					       connector->edid_blob_ptr->base.id);
+
+	return ret;
+}
 
 int drm_mode_connector_property_set_ioctl(struct drm_device *dev,
 				       void *data, struct drm_file *file_priv)
