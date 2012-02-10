08bd70649c6d414492ae43a74f290a5054085197
raichoo <raichoo@googlemail.com>
2012-02-11 00:57:52 +0100

    extern drm_mode_set_name

--- usr/src/uts/common/io/drm/drm_modes.h
+++ usr/src/uts/common/io/drm/drm_modes.h
@@ -2,4 +2,7 @@ extern struct drm_display_mode *drm_mode_duplicate(struct drm_device *dev,
 					    const struct drm_display_mode *mode);
 
 extern int drm_mode_vrefresh(const struct drm_display_mode *mode);
+
 extern int drm_mode_hsync(const struct drm_display_mode *mode);
+
+extern void drm_mode_set_name(struct drm_display_mode *mode);
